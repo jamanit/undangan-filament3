5e1e0d477da56c440cd5af8ffb86cfaa2ff14727
Jaman IT <rikidavidtra.2310@gmail.com>
2025-03-16 15:11:53 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,11 +25,13 @@
                 </div>
             </div>
 
-            <a href="#services" class="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white animate-bounce">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-                </svg>
-            </a>
+            <div class="absolute bottom-4 flex justify-center w-full">
+                <a href="#services" class="text-white animate-bounce">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </a>
+            </div>
         </section>
 
         <section class="relative">
