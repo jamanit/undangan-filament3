5b09b813f18221c11450377dcdb12edc9b17622c
Jaman IT <rikidavidtra.2310@gmail.com>
2025-03-26 09:29:52 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -6,34 +6,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/symlink', function () {
-    // Cek jalur target
-    $target = $_SERVER['DOCUMENT_ROOT'];
-    // $link = $_SERVER['DOCUMENT_ROOT'] . "/storage";
-    $link = $_SERVER['DOCUMENT_ROOT'] . '/../public_html/undangbae.site';
-
-
-    print_r($target);
-    exit();
-
-    // Debug: Cek apakah jalur target valid
-    if ($target === false) {
-        die('Jalur target tidak ditemukan.');
-    }
-
-    // Hapus symlink lama jika ada
-    if (is_link($link)) {
-        unlink($link);
-    }
-
-    // Buat symlink baru
-    if (symlink($target, $link)) {
-        echo "Symbolic link berhasil dibuat: " . $link;
-    } else {
-        echo "Gagal membuat symbolic link.";
-    }
-});
-
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
 Route::post('/inboxes/store', [App\Http\Controllers\HomeController::class, 'inbox_store'])->name('inboxes.store');
 Route::get('/templates', [App\Http\Controllers\HomeController::class, 'template'])->name('templates.index');
